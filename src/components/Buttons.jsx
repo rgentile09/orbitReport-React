86459545c811059ed9e1d35ced3104458a438849
Ronnie Gentile <rgentile09@yahoo.com>
2024-03-18 20:14:29 -0500

    Assignment 6 complete!

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,18 @@
+import satData from "./satData";
+import Table from "./Table";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+})}
+
+      <button onClick={(Table) => setSat(satData)}>Show All Orbits</button>
     </div>
   );
 };
